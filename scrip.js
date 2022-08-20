b1d3fb603bd60d4d0bb3d5897cb34d4bea9c945f
Samfoxfox <110495533+Samfoxfox@users.noreply.github.com>
2022-08-20 20:50:28 -0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,19 @@
+AOS.init({
+    duration: 800,
+    once: true
+  });
+  const article = document.querySelector("#content");
+  const button = document.querySelector("#read-more");
+ 
+ button.addEventListener("click", readMore);
+ 
+ function readMore() {
+      if (article.className == "open") {
+        // Ler menos
+      article.className = "";
+      button.innerHTML = "Show more"; // Mostrar mais
+    } else {
+      article.className = "open";
+      button.innerHTML = "Show less"; // Mostrar menos
+    }
+ }
